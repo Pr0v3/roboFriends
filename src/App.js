64165eea189d51895087926c4ece116d74cd29e1
unknown <cobsmr@gmail.com>
2019-12-23 02:51:01 +0700

    updating font & implement hooks

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import Card from "./Card";
 import SearchBox from "./SearchBox";
 import CardList from "./CardList";
 import { robots } from "./robots";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: ""
     };
+    console.log(1);
   }
 
+  componentDidMount() {
+    this.setState({ robots: robots });
+    console.log(2);
+  }
   onSearchChange = event => {
     this.setState({ searchField: event.target.value });
   };
@@ -22,9 +28,11 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchField.toLowerCase());
     });
+    console.log(3);
+
     return (
       <div className="tc">
-        <h1>RoboFriends</h1>
+        <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
